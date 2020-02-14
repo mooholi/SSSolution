c145b35bffbfe820886360d32a8364dc7f54c06e
Morgana Oliveira <morganaboos@hotmail.com>
2020-02-14 17:03:56 -0300

    Adicionar arquivos de projeto.

---
+++ MVCWebPresentationLayer/Controllers/ProdutoController.cs
@@ -0,0 +1,39 @@
+﻿using MVCWebPresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebPresentationLayer.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Testar()
+        {
+            return View();
+        }
+
+
+        public ActionResult Cadastrar ()
+        {
+            return View();
+        }
+        [HttpPost]
+        //Significa que o método abaixo recebe dados 
+
+        public ActionResult Cadastrar(ProdutoViewModel viewModel)
+        {
+
+            return View();
+        }
+
+
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
