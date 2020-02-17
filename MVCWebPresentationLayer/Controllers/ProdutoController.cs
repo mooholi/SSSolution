345d8a171e50cd3c4ffb47cd91433cdd550467bf
Morgana Oliveira <morganaboos@hotmail.com>
2020-02-17 14:49:33 -0300

    Criamos um mockBLL para poder cadastrar

--- MVCWebPresentationLayer/Controllers/ProdutoController.cs
+++ MVCWebPresentationLayer/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using MVCWebPresentationLayer.Models;
+﻿using MVCWebPresentationLayer.Mock;
+using MVCWebPresentationLayer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,35 @@ namespace MVCWebPresentationLayer.Controllers
         {
             return View();
         }
+
+        //HttpPost -> Significa que este método irá atender uma requisção 
+        //feita no back-end via click do botão em um form com httppost 
+        //da View Testar 
+        [HttpPost]
+        public ActionResult Testar(ProdutoViewModel viewModel)
+        {
+            //O objeto ViewModel virá preenchido de acordo com os names dos inputs
+            //do form que teve seu botão clicado
+            ProdutoMockBLL mockBLL = new ProdutoMockBLL();
+            try
+            {
+                //Esta linha pode lançar uma exception , logo ela 
+                // deve estar entre um bloco try
+                mockBLL.Cadastrar(viewModel);
+
+                //Se chegou e não deu Exception, redirecione o usuário para a página inicial! :)
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                //Se chegou aqui, o método Cadastrar do MockBLL deu erro :(
+
+            }
+
+
+            return View();
+        }
+
 
 
         public ActionResult Cadastrar ()
