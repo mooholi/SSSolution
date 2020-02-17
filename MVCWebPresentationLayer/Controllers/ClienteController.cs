665f9dfc62611b5fa76611c3a37164c8f62f210e
Morgana Oliveira <morganaboos@hotmail.com>
2020-02-17 17:06:18 -0300

    Atividade Feita em sala ClienteController Cadastrar ClienteViewModel

---
+++ MVCWebPresentationLayer/Controllers/ClienteController.cs
@@ -0,0 +1,30 @@
+﻿using MVCWebPresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebPresentationLayer.Controllers
+{
+    public class ClienteController : Controller
+    {
+       public ActionResult Cadastrar()
+       {
+            return View();
+       }
+        [HttpPost]
+        public ActionResult Cadastrar(ClienteViewModel viewModel)
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
